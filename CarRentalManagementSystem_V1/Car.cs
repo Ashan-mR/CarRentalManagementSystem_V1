69380b1368741c573fa485bef03f7c90668aed96
ut03301 <ut03301@UNICOMTIC7>
2024-10-11 14:08:39 +0530

    Add project files.

---
+++ CarRentalManagementSystem_V1/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalManagementSystem_V1
+{
+    public class Car
+    {
+       
+        private string carId { get; set; }
+        private string brand { get; set; }
+        private string Model { get; set; }
+        private decimal rentalPrice { get; set; }
+
+        public Car(string carId, string brand, string model, decimal rentalPrice)
+        {
+            this.carId = carId;
+            this.brand = brand;
+            Model = model;
+            this.rentalPrice = rentalPrice;
+        }
+
+        public override string ToString()
+        {
+          
+            return $"ID: {CarId}\n, Brand: {Brand}\n, Model: {Model}\n, RentalPrice: {RentalPrice}\n";
+        }
+
+    }
